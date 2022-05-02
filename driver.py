23cfc468c16f7b9554549fffb078d37471966f77
JayC319 <jwei2001@gmail.com>
2022-05-02 23:53:52 +0800

    update 0502

--- driver.py
+++ driver.py
@@ -1,7 +1,373 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 30 20:22:35 2022
+# In[1]
+import os 
+if not os.path.isfile('data.zip'):
+    !wget https://raw.githubusercontent.com/zcakhaa/DeepLOB-Deep-Convolutional-Neural-Networks-for-Limit-Order-Books/master/data/data.zip
+    !unzip -n data.zip
+    print('data downloaded.')
+else:
+    print('data already existed.')
 
-@author: asus
-"""
+# In[2]
+import pandas as pd
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from tqdm import tqdm 
+from sklearn.metrics import accuracy_score, classification_report
 
+import torch
+import torch.nn.functional as F
+from torch.utils import data
+from torchinfo import summary
+import torch.nn as nn
+import torch.optim as optim
+
+
+
+# In[3]
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+
+print(torch.cuda.is_available())
+
+
+print(torch.cuda.device_count())
+
+
+print(torch.cuda.current_device())
+
+
+print(torch.cuda.device(0))
+
+
+print(torch.cuda.get_device_name(0))
+
+# In[4]
+def prepare_x(data):
+    df1 = data[:40, :].T
+    return np.array(df1)
+
+def get_label(data):
+    lob = data[-5:, :].T
+    return lob
+
+def data_classification(X, Y, T):
+    [N, D] = X.shape
+    df = np.array(X)
+    
+    dY = np.array(Y)
+    
+    dataY = dY[T - 1:N]
+    
+    dataX = np.zeros((N - T + 1, T, D))
+    for i in range(T, N + 1):
+        dataX[i - T] = df[i - T:i, :]
+        
+    return dataX, dataY
+
+def torch_data(x, y):
+    x = torch.from_numpy(x)
+    x = torch.unsqueeze(x, 1)
+    y = torch.from_numpy(y)
+    y = F.one_hot(y, num_classes=3)
+    return  x, y
+
+# In[5]
+class Dataset(data.Dataset):
+    def __init__(self, data, k, num_classes, T):
+        self.k = k
+        self.num_classes = num_classes
+        self.T = T
+        
+        x = prepare_x(data)
+        y = get_label(data)
+        x, y = data_classification(x, y, self.T)
+        y = y[:, self.k] - 1
+        self.length = len(x)
+        
+        x = torch.from_numpy(x)
+        self.x = torch.unsqueeze(x, 1)
+        self.y = torch.from_numpy(y)
+    
+    def __len__(self):
+        """Denotes the total number of samples"""
+        return self.length
+    
+    def __getitem__(self, index):
+        """Generates samples of data"""
+        return self.x[index], self.y[index]
+    
+# In[6]
+dec_data = np.loadtxt('Train_Dst_NoAuction_DecPre_CF_7.txt')
+dec_train = dec_data[:, :int(np.floor(dec_data.shape[1] * 0.8))]
+dec_val = dec_data[:, int(np.floor(dec_data.shape[1] * 0.8)):]
+
+dec_test1 = np.loadtxt('Test_Dst_NoAuction_DecPre_CF_7.txt')
+dec_test2 = np.loadtxt('Test_Dst_NoAuction_DecPre_CF_8.txt')
+dec_test3 = np.loadtxt('Test_Dst_NoAuction_DecPre_CF_9.txt')
+dec_test = np.hstack((dec_test1, dec_test2, dec_test3))
+
+print(dec_train.shape, dec_val.shape, dec_test.shape)
+
+# In[7]
+batch_size = 32
+
+dataset_train = Dataset(data = dec_train, k=4, num_classes=3, T=100)
+dataset_val = Dataset(data=dec_val, k=4, num_classes=3, T=100)
+dataset_test = Dataset(data=dec_test, k=4, num_classes=3, T=100)
+
+train_loader = torch.utils.data.DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=dataset_test, batch_size=batch_size, shuffle=False)
+
+print(dataset_train.x.shape, dataset_train.y.shape)
+
+# In[8]
+
+tmp_loader = torch.utils.data.DataLoader(dataset=dataset_train, batch_size=1, shuffle=True)
+
+for x, y in tmp_loader:
+    print(x)
+    print(y)
+    print(x.shape, y.shape)
+    break
+
+# In[9]
+
+class deeplob(nn.Module):
+    def __init__(self, y_len):
+        super().__init__()
+        self.y_len = y_len
+        
+        # convolution blocks
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(1,2), stride=(1,2)),
+            nn.LeakyReLU(negative_slope=0.01),
+#             nn.Tanh(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1,2), stride=(1,2)),
+            nn.Tanh(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.Tanh(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.Tanh(),
+            nn.BatchNorm2d(32),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1,10)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+        )
+        
+        # inception moduels
+        self.inp1 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        self.inp2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        self.inp3 = nn.Sequential(
+            nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0)),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        
+        # lstm layers
+        self.lstm = nn.LSTM(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(64, self.y_len)
+
+    def forward(self, x):
+        # h0: (number of hidden layers, batch size, hidden size)
+        h0 = torch.zeros(1, x.size(0), 64).to(device)
+        c0 = torch.zeros(1, x.size(0), 64).to(device)
+    
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        
+        x_inp1 = self.inp1(x)
+        x_inp2 = self.inp2(x)
+        x_inp3 = self.inp3(x)  
+        
+        x = torch.cat((x_inp1, x_inp2, x_inp3), dim=1)
+        
+#         x = torch.transpose(x, 1, 2)
+        x = x.permute(0, 2, 1, 3)
+        x = torch.reshape(x, (-1, x.shape[1], x.shape[2]))
+        
+        x, _ = self.lstm(x, (h0, c0))
+        x = x[:, -1, :]
+        x = self.fc1(x)
+        forecast_y = torch.softmax(x, dim=1)
+        
+        return forecast_y
+    
+# In[10]
+
+model = deeplob(y_len = dataset_train.num_classes)
+model.to(device)
+
+
+# In[11]
+
+summary(model, (1, 1, 100, 40))
+
+# In[12]
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+#%%
+
+## model training for the below cells
+
+# In[13]
+# A function to encapsulate the training loop
+def batch_gd(model, criterion, optimizer, train_loader, test_loader, epochs):
+    
+    train_losses = np.zeros(epochs)
+    test_losses = np.zeros(epochs)
+    best_test_loss = np.inf
+    best_test_epoch = 0
+
+    for it in tqdm(range(epochs)):
+        
+        model.train()
+        t0 = datetime.now()
+        train_loss = []
+        for inputs, targets in train_loader:
+            # move data to GPU
+            inputs, targets = inputs.to(device, dtype=torch.float), targets.to(device, dtype=torch.int64)
+            # print("inputs.shape:", inputs.shape)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # Forward pass
+            # print("about to get model output")
+            outputs = model(inputs)
+            # print("done getting model output")
+            # print("outputs.shape:", outputs.shape, "targets.shape:", targets.shape)
+            loss = criterion(outputs, targets)
+            # Backward and optimize
+            # print("about to optimize")
+            loss.backward()
+            optimizer.step()
+            train_loss.append(loss.item())
+        # Get train loss and test loss
+        train_loss = np.mean(train_loss) # a little misleading
+    
+        model.eval()
+        test_loss = []
+        for inputs, targets in test_loader:
+            inputs, targets = inputs.to(device, dtype=torch.float), targets.to(device, dtype=torch.int64)      
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            test_loss.append(loss.item())
+        test_loss = np.mean(test_loss)
+
+        # Save losses
+        train_losses[it] = train_loss
+        test_losses[it] = test_loss
+        
+        if test_loss < best_test_loss:
+            torch.save(model, './best_val_model_pytorch')
+            best_test_loss = test_loss
+            best_test_epoch = it
+            print('model saved')
+
+        dt = datetime.now() - t0
+        print(f'Epoch {it+1}/{epochs}, Train Loss: {train_loss:.4f}, \
+          Validation Loss: {test_loss:.4f}, Duration: {dt}, Best Val Epoch: {best_test_epoch}')
+
+    return train_losses, test_losses
+
+# In[14]
+train_losses, val_losses = batch_gd(model, criterion, optimizer, 
+                                    train_loader, val_loader, epochs=50)
+
+# In[15]
+
+plt.figure(figsize=(15,6))
+plt.plot(train_losses, label='train loss')
+plt.plot(val_losses, label='validation loss')
+plt.legend()
+
+#%%
+
+# model testing for below cells
+
+# In[16]
+model = torch.load('best_val_model_pytorch')
+
+n_correct = 0.
+n_total = 0.
+for inputs, targets in test_loader:
+    # Move to GPU
+    inputs, targets = inputs.to(device, dtype=torch.float), targets.to(device, dtype=torch.int64)
+
+    # Forward pass
+    outputs = model(inputs)
+    
+    # Get prediction
+    # torch.max returns both max and argmax
+    _, predictions = torch.max(outputs, 1)
+
+    # update counts
+    n_correct += (predictions == targets).sum().item()
+    n_total += targets.shape[0]
+
+test_acc = n_correct / n_total
+print(f"Test acc: {test_acc:.4f}")
+
+# In[17]
+
+# model = torch.load('best_val_model_pytorch')
+all_targets = []
+all_predictions = []
+
+for inputs, targets in test_loader:
+    # Move to GPU
+    inputs, targets = inputs.to(device, dtype=torch.float), targets.to(device, dtype=torch.int64)
+
+    # Forward pass
+    outputs = model(inputs)
+    
+    # Get prediction
+    # torch.max returns both max and argmax
+    _, predictions = torch.max(outputs, 1)
+
+    all_targets.append(targets.cpu().numpy())
+    all_predictions.append(predictions.cpu().numpy())
+
+all_targets = np.concatenate(all_targets)    
+all_predictions = np.concatenate(all_predictions)
+
+# In[18]
